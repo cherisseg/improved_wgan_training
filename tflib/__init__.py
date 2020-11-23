ae8b7ca7fcf4d86a901a44fb7c695ffc4cbac43e
cherisseg <73181860+cherisseg@users.noreply.github.com>
2020-11-23 13:19:48 +0200

    Update __init__.py

--- tflib/__init__.py
+++ tflib/__init__.py
@@ -107,8 +107,8 @@ def print_model_settings(locals_):
 
 
 def print_model_settings_dict(settings):
-    print "Settings dict:"
+    print ("Settings dict:")
     all_vars = [(k,v) for (k,v) in settings.items()]
     all_vars = sorted(all_vars, key=lambda x: x[0])
     for var_name, var_value in all_vars:
-        print "\t{}: {}".format(var_name, var_value)
+        print ("\t{}: {}".format(var_name, var_value))
